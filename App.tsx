1b2b24aa0b0399dadb6a71e6f92a60b990e8b613
thien31072 <thiencn01012004@gmail.com>
2024-10-11 22:23:12 +0700

    Update App.tsx
    code nè Fuck

--- App.tsx
+++ App.tsx
@@ -76,18 +76,17 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Thiên Lý ơi">
+            Edit <Text style={styles.highlight}>App.tsx</Text> Ngàn câu hauws chắng đc gì cuối cùng phái chia ly
           </Section>
-          <Section title="See Your Changes">
+          <Section title="ĐOm đóm">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            delll bt nói j hionw
           </Section>
           <LearnMoreLinks />
         </View>
